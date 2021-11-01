69518c2712cb4588905fe1b5098c3c5a95a35aa7
“Chej <chejahamed@gmail.com>
2021-11-01 20:41:25 +0000

    Add header structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 
 import './App.css';
-
+import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
+      <Header/>
       <p>Hello Word</p>
     </div>
   );
