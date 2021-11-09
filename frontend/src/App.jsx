fb0ee01991b3888152e7224ac9ebabca08387ce9
Chej <chejahamed@gmail.com>
2021-11-09 20:34:52 +0000

    Create Dashboad page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Dashboard />
       <Footer />
     </div>
   );
