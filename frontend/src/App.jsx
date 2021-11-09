97c6fb332d86e80219d45198b24b6b7feaf2cd92
Chej <chejahamed@gmail.com>
2021-11-09 20:32:34 +0000

    Import header and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello Word</p>
+      <Header/>
+      <Footer />
     </div>
   );
 }
